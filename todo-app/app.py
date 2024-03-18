6088be007a177f08fc925b5075d07ed17040ba6c
Grant Achuzia <47652337+GAchuzia@users.noreply.github.com>
2024-03-17 20:59:06 -0400

    feat: Update flask app to use database.py to get data from the db

--- todo-app/app.py
+++ todo-app/app.py
@@ -1,17 +1,28 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
+from database import connect_to_db, get_db
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template("index.html")
 
-@app.route('/add_task')
+@app.route("/", methods = ["POST", "GET"])
+def index():
+    db = get_db()
+    tasks_getter = db.execute("select * from todolist")
+    all_db_tasks = tasks_getter.fetchall()
+    return render_template("index.html", all_db_tasks = all_db_tasks)
+
+
+@app.route("/add_task", methods=["POST", "GET"])
 def add_task():
     if request.method == "POST":
-        user_task = request.form['user_task']
-        
-    return render_template("index.html", methods=["POST","GET"])
+        user_task = request.form["user_task"]
+        db = get_db()
+        db.execute("insert into todolist (task) values (?)", [user_task])
+        db.commit()
+        return redirect(url_for("index"))
 
-if __name__ == '__main__':
-    app.run(debug = True)
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
